86e1bfc7adcbd8af1cdf4923b4b998eace2b52ff
Rabbi <Rabbyislam7777@gmail.com>
2023-02-27 11:00:31 +0600

    checkout done

--- app/src/main/java/com/rabbi/e_commercewithpaymentsystem/PaymentActivity.java
+++ app/src/main/java/com/rabbi/e_commercewithpaymentsystem/PaymentActivity.java
@@ -2,13 +2,22 @@ package com.rabbi.e_commercewithpaymentsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.rabbi.e_commercewithpaymentsystem.databinding.ActivityPaymentBinding;
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
 
-public class PaymentActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+public class PaymentActivity extends AppCompatActivity implements PaymentResultListener {
 
     ActivityPaymentBinding binding;
+    double amount = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +29,64 @@ public class PaymentActivity extends AppCompatActivity {
         if (getSupportActionBar() != null){
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-        double amount = 0.0;
+
         amount = getIntent().getDoubleExtra("amount",0.0);
         binding.subTotal.setText(amount+"$");
+
+        binding.payBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paymentMethod();
+            }
+        });
+
+
     }
-}
+
+    private void paymentMethod() {
+
+        Checkout checkout = new Checkout();
+
+        final Activity activity = PaymentActivity.this;
+
+        try {
+            JSONObject options = new JSONObject();
+            //Set Company Name
+            options.put("name", "My E-Commerce App");
+            //Ref no
+            options.put("description", "Reference No. #123456");
+            //Image to be display
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+            //options.put("order_id", "order_9A33XWu170gUtm");
+            // Currency type
+            options.put("currency", "USD");
+            //double total = Double.parseDouble(mAmountText.getText().toString());
+            //multiply with 100 to get exact amount in rupee
+            amount = amount * 100;
+            //amount
+            options.put("amount", amount);
+            JSONObject preFill = new JSONObject();
+            //email
+            preFill.put("email", "developer.kharag@gmail.com");
+            //contact
+            preFill.put("contact", "7489347378");
+
+            options.put("prefill", preFill);
+
+            checkout.open(activity, options);
+        } catch (Exception e) {
+            Log.e("TAG", "Error in starting Razorpay Checkout", e);
+        }
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        Toast.makeText(this, "Payment Successful", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+
+        Toast.makeText(this, "Payment Cancel", Toast.LENGTH_SHORT).show();
+    }
+    }
